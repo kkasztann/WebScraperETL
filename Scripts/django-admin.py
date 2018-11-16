f64576bc7bbe9372e934f02e88fdd597e6448bb5
Daniel Slowik <kkasztann@gmail.com>
2018-11-16 16:34:12 +0100

    deleted virtual env folders

--- Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\kasztan\desktop\studia\2stopi~1\semest~1\hurtow~1\wiczen~1\project\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
