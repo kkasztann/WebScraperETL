f533afb86803b2235a4b1263c1a99c0f4dc0b82b
Daniel Slowik <kkasztann@gmail.com>
2018-12-14 04:22:53 +0100

    rel produuct-prodDetails

--- Application/WebScraperETL/admin.py
+++ Application/WebScraperETL/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Opinion
 from .models import Product
+from .models import ProductDetail
+from .models import Opinion
 
-admin.site.register(Opinion)
 admin.site.register(Product)
+admin.site.register(ProductDetail)
+admin.site.register(Opinion)
--- Application/WebScraperETL/models.py
+++ Application/WebScraperETL/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+
+class Product(models.Model):
+    productID = models.CharField(max_length=50)
+    productName = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.productID + ' | ' + self.productName
+
+class ProductDetail(models.Model):
+    parameter = models.CharField(max_length=80)
+    value = models.CharField(max_length=80)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.product.productID+ ' | ' + self.parameter
+
+class ProductID(models.Model):
+    productID = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.productID
+
 class Opinion(models.Model):
     productID = models.CharField(max_length=50)
     username = models.CharField(max_length=50)
@@ -8,18 +30,3 @@ class Opinion(models.Model):
 
     def __str__(self):
         return self.productID + ' | ' + self.username
-
-class Product(models.Model):
-    productID = models.CharField(max_length=50)
-    productName = models.CharField(max_length=50)
-    parameter = models.CharField(max_length=80)
-    value = models.CharField(max_length=80)
-
-    def __str__(self):
-        return self.productID + ' | ' + self.parameter
-
-class ProductID(models.Model):
-    productID = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.productID
