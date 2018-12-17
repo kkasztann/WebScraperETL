c656af6e3763994715dc8ce29b6528a542e4d4f7
Daniel Slowik <kkasztann@gmail.com>
2018-12-17 01:13:25 +0100

    added second click block

---
+++ Application/static/js/main.js
@@ -0,0 +1,6 @@
+function buttonOff() {
+    setTimeout(function(){
+         document.getElementById("button0").disabled = true;
+         document.getElementById("button1").disabled = true;
+         }, 50);
+}
